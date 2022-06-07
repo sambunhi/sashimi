9a45005720336ff189c6102cecf9e95c8472e7e1
Karl Lin <linroex@coder.tw>
2022-06-07 19:43:05 +0800

    change saveCrawlerLinks method to POST

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::prefix('v1')->controller(ApiController::class)->group(function () {
     Route::get('/crawler', 'getSystemInfo');
     Route::get('/trends', 'getTrends');
     Route::get('/articles', 'getArticles');
-    Route::put('/crawler', 'saveCrawlerLinks');
+    Route::post('/article', 'saveCrawlerLinks');
     Route::put('/article', 'saveArticleKeywords');
 });
